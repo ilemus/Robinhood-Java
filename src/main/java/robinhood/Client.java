64d8cf19f275806f328e268b5e89c03b44399739
Isaac Lemus <3820277@mobis.co.kr>
2019-07-09 16:58:49 -0400

    Fixing test

--- src/main/java/robinhood/Client.java
+++ src/main/java/robinhood/Client.java
@@ -1,12 +1,18 @@
 package robinhood;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Client {
@@ -14,6 +20,7 @@ public class Client {
 	private boolean INSECURE = false;
 	private static final String VERSION = "1.0";
 	private static Client INSTANCE = new Client();
+	private static final int STREAM_BUFFER_SIZE = 128;
 	private Client() {
 		
 	}
@@ -22,13 +29,73 @@ public class Client {
 		return INSTANCE;
 	}
 	
-	private static HttpsURLConnection getRequest(String url, String method, List<String[]> headers) throws MalformedURLException, IOException {
+	HttpsURLConnection makeConnection(String url, String method, List<String[]> headers) throws MalformedURLException, IOException {
 		HttpsURLConnection conn = (HttpsURLConnection) new URL(url).openConnection();
-		
+		conn.setRequestMethod(method);
+		for (String[] header : headers) {
+			conn.setRequestProperty(header[0], header[1]);
+		}
 		return conn;
 	}
 	
-	private static Object makeRequest(HttpsURLConnection conn, JSONObject obj) {
-		return new Object();
+	// RESTful request
+	JSONObject makeRequest(HttpsURLConnection conn, JSONObject obj) {
+		// Posting data to server
+		String body = obj.toString();
+		OutputStream os;
+		try {
+			os = conn.getOutputStream();
+			BufferedOutputStream bos = new BufferedOutputStream(os);
+			bos.write(body.getBytes());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// Reading data from server
+		InputStream is;
+		try {
+			is = conn.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			StringBuilder sb = new StringBuilder();
+			char[] buffer = new char[STREAM_BUFFER_SIZE];
+			int read = 0;
+			while ((read = isr.read(buffer)) > 0) {
+				sb.append(buffer, 0, read);
+			}
+			JSONObject response = new JSONObject(sb.toString());
+			return response;
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	// RESTful request
+	JSONObject makeRequest(HttpsURLConnection conn) {
+		// Reading data from server
+		InputStream is;
+		try {
+			is = conn.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			StringBuilder sb = new StringBuilder();
+			char[] buffer = new char[STREAM_BUFFER_SIZE];
+			int read = 0;
+			while ((read = isr.read(buffer)) > 0) {
+				sb.append(buffer, 0, read);
+			}
+			JSONObject response = new JSONObject(sb.toString());
+			return response;
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
--- src/test/java/robinhood/RobinhoodTest.java
+++ src/test/java/robinhood/RobinhoodTest.java
@@ -1,10 +1,42 @@
 package robinhood;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.json.JSONObject;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class RobinhoodTest {
     @Test public void testSomeLibraryMethod() {
-    	Client classUnderTest = Client.getInstance();
+    	Client client = Client.getInstance();
+    	List<String[]> headers = new ArrayList<>();
+    	HttpsURLConnection conn = null;
+		try {
+			conn = client.makeConnection("https://www.google.com", "GET", headers);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			assertTrue(false);
+			return;
+		}
+    	int responseCode = 0;
+    	try {
+			responseCode = conn.getResponseCode();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			assertTrue(false);
+			return;
+		}
+    	System.out.println(responseCode);
+    	assertEquals(responseCode, 200);
+    	
+    	JSONObject obj = client.makeRequest(conn);
+    	assertEquals(obj, null);
     }
 }
